89bb7a96eb7fffd79830d84437974777b61e2103
Elena Kvasko <lenakvasko@gmail.com>
2023-01-18 19:25:48 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+function createGalleryMarkap(items) {
+  const markap = galleryItems
+    .map(({ preview, original, description }) => {
+      return;
+      `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+  console.log(markap);
+}
