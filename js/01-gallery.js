d298f0509c452092cd2c8d3cf057c597ec35d85c
Elena Kvasko <lenakvasko@gmail.com>
2023-01-19 10:44:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-function createGalleryMarkap(items) {
-  const markap = galleryItems
+//console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImageMarkap(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryImageMarkap(items) {
+  return galleryItems
     .map(({ preview, original, description }) => {
-      return;
-      `<div class="gallery__item">
+      return `
+      <div class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </div>`;
     })
     .join('');
-  console.log(markap);
 }
