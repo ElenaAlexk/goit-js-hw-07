707e3a411c6d42179f72280029bb69fa35f18fb5
Elena Kvasko <lenakvasko@gmail.com>
2023-01-19 12:08:17 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,14 +4,17 @@ import { galleryItems } from './gallery-items.js';
 //console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryImageMarkap(galleryItems);
+
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function createGalleryImageMarkap(items) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
       <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -22,4 +25,12 @@ function createGalleryImageMarkap(items) {
 </div>`;
     })
     .join('');
+}
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const isGalleryImage = event.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+  const currentImgUrl = event.target.dataset.source;
 }
