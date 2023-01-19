0bd1cfc9ecb70af2b9264b252ab2fa43c9a56b28
Elena Kvasko <lenakvasko@gmail.com>
2023-01-19 19:04:48 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -36,6 +36,24 @@ function onGalleryContainerClick(event) {
 
   const currentImgUrl = event.target.dataset.source;
 
-  const instance = basicLightbox.create(`<img src="${currentImgUrl}" width="1280" height="auto">`);
+  const instance = basicLightbox.create(
+    `<img src="${currentImgUrl}" width="1280" height="auto">`,
+
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
   instance.show();
+
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === 'ESC_KEY_CODE') {
+      instance.close();
+    }
+  }
 }
