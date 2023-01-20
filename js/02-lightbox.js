9b9f31858e7009f1208b544520864d5401300883
Elena Kvasko <lenakvasko@gmail.com>
2023-01-20 08:20:35 +0200

    Update html and js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryImageMarkap(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryImageMarkap(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const isGalleryImage = event.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const currentImgUrl = event.target.dataset.source;
+}
